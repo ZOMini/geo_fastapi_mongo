3fd0d7b7ac724f63acd9a5b7e242407eab9cee74
ZOMini <zarikacr@gmail.com>
2022-09-09 00:47:48 +0300

    v 1.0

--- geo_api/script.py
+++ geo_api/script.py
@@ -3,8 +3,8 @@ import runpy
 # import sys
 
 if __name__ == '__main__':
-    runpy.run_path('geo_api/data/data_txt_to_sql.py', run_name='__main__')
-    runpy.run_path('geo_api/data/index_db.py', run_name='__main__')
+    runpy.run_path('data/data_txt_to_sql.py', run_name='__main__')
+    runpy.run_path('data/index_db.py', run_name='__main__')
 
 # sys.argv = ['', 'runserver']
 # runpy.run_path('./manage.py', run_name='__main__')
